a5dfbcc50aa9ec1af8a4b52c83d17827439e9400
DiegoMoreno0640 <diegomoreno.pro45@gmail.com>
2022-03-10 15:08:37 +0000

    Tailwind test

--- InsaneViper-WebAppFront/src/App.js
+++ InsaneViper-WebAppFront/src/App.js
@@ -1,14 +1,14 @@
 //import logo from './logo.jpg';
 import React from "react";
 import Navbar from "./components/navbar/navbar.js"
+import Example from "./components/navbar/navbar.js"
 import './App.css';
 
 
 class App extends React.Component{
   render(){
     return (
-      <div class="app">
-        <Navbar></Navbar>
+      <div class="container">
       </div>
     )
   }
